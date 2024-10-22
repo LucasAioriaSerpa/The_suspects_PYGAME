c49cabb0a8db0395b4180a48558a6004f346f3e7
Lucas Aioria Serpa <136469417+Garudaakuma@users.noreply.github.com>
2024-10-22 10:16:20 -0300

    json documentation and other shit

--- src/SCRIPTS/load_json.py
+++ src/SCRIPTS/load_json.py
@@ -3,6 +3,28 @@ import json
 #imports internal
 ...
 class CONFIG():
+    """@CONFIG class.
+
+    ...
+    Attributes
+    ----------
+        @json_config : is a dictionary with the complete json DATA
+        @game_config : is a dictionary with values for the game
+            "CAPTION": "NAME OF THE GAME",
+            "RESOLUTION": [WIDTH, HEIGHT],
+            "BACKGROUND": "BACKGROUND COLOR HEX",
+            "FPS": INTEGER FRAMES PER SECOND,
+        @paths : is a dictionary with paths of the files
+            "PATH-DEFAULT": "src",
+            "PATH-IMAGES": "src/IMAGES/",
+            "PATH-IMAGES-mouse": "src/IMAGES/mouse/",
+            "PATH-IMAGES-menu": "src/IMAGES/menu/",
+            "PATH-IMAGES-characteres": "src/IMAGES/characteres/",
+            "PATH-IMAGES-player": "src/IMAGES/player/",
+            "PATH-FONTS": "src/FONTS/",
+            "PATH-AUDIOS": "src/AUDIOS/",
+            "PATH-JSON": "src/JSON/"
+    """
     def __init__(self):
         self.json_config = dict
         self.game_config = {
@@ -23,16 +45,12 @@ class CONFIG():
             "PATH-JSON": ...
         }
     def create_config(self):
-        '''reads and loads DATA from json to the dictionary'''
         #? loads game_config.json to json_config dict
-        with open('src/JSON/game_config.json', 'r') as file:
-            self.json_config = json.load(file)
+        with open('src/JSON/game_config.json', 'r') as file: self.json_config = json.load(file)
         print(f"\nJSON DATA: {self.json_config}\n")
         #? loads game_config dict with json_config
-        for key in self.game_config.keys():
-            self.game_config[key] = self.json_config["configs"][key]
+        for key in self.game_config.keys(): self.game_config[key] = self.json_config["configs"][key]
         print(f"game_config DATA: {self.game_config}\n")
         #? loads paths dict with json_config
-        for key in self.paths.keys():
-            self.paths[key] = self.json_config["paths"][key]
+        for key in self.paths.keys(): self.paths[key] = self.json_config["paths"][key]
         print(f"paths DATA: {self.paths}")
