a76eedd837acfc0380fb0f62a51555f404bbd658
Garudaakuma <136469417+Garudaakuma@users.noreply.github.com>
2024-11-01 00:34:20 -0300

    apply types npcs

--- SRC/SCRIPTS/generate_case.py
+++ SRC/SCRIPTS/generate_case.py
@@ -12,34 +12,35 @@ class CASE():
         self.CASO = {
             "DEATH_CASE": ...,
             "DEATH_PLACE": ...,
-            "DIALOG_PLACE": ...,
         }
-        self.EVIDENCES = {
-            "TRURTHS": [...],
-            "CONFLICTS": [...],
-            "FALSES": [...]
-        }
-    def set_types(self, npc: int, npc_str: str):
+        self.DIALOG_
+    def set_types(self, npc: int):
         match self.CASO["DEATH_CASE"]:
             case "Envenenado":
                 match npc:
                     case 0:
                         self.NPCS[npc]["TYPE"] = self.SCENE_VALUES["CASO"]["NPC_TYPES"][2]
+                        npc_str = f"{f"{self.NPCS[npc]["NAME"]}":<20}||     {f"{self.NPCS[npc]["DESCRIPTION"]}":<90}||      {self.NPCS[npc]["TYPE"]}"
                         print(f"0-Vitima:                 {npc_str}")
                     case 1:
                         self.NPCS[npc]["TYPE"] = self.SCENE_VALUES["CASO"]["NPC_TYPES"][0]
+                        npc_str = f"{f"{self.NPCS[npc]["NAME"]}":<20}||     {f"{self.NPCS[npc]["DESCRIPTION"]}":<90}||      {self.NPCS[npc]["TYPE"]}"
                         print(f"1-Assassino:              {npc_str}")
                     case 2:
                         self.NPCS[npc]["TYPE"] = self.SCENE_VALUES["CASO"]["NPC_TYPES"][1]
+                        npc_str = f"{f"{self.NPCS[npc]["NAME"]}":<20}||     {f"{self.NPCS[npc]["DESCRIPTION"]}":<90}||      {self.NPCS[npc]["TYPE"]}"
                         print(f"2-Testemunha:             {npc_str}")
                     case 3:
                         self.NPCS[npc]["TYPE"] = self.SCENE_VALUES["CASO"]["NPC_TYPES"][1]
+                        npc_str = f"{f"{self.NPCS[npc]["NAME"]}":<20}||     {f"{self.NPCS[npc]["DESCRIPTION"]}":<90}||      {self.NPCS[npc]["TYPE"]}"
                         print(f"3-Suspeito principal:     {npc_str}")
                     case 4:
                         self.NPCS[npc]["TYPE"] = self.SCENE_VALUES["CASO"]["NPC_TYPES"][1]
+                        npc_str = f"{f"{self.NPCS[npc]["NAME"]}":<20}||     {f"{self.NPCS[npc]["DESCRIPTION"]}":<90}||      {self.NPCS[npc]["TYPE"]}"
                         print(f"4-Cúmplice/testemunha:    {npc_str}")
                     case 5:
                         self.NPCS[npc]["TYPE"] = self.SCENE_VALUES["CASO"]["NPC_TYPES"][1]
+                        npc_str = f"{f"{self.NPCS[npc]["NAME"]}":<20}||     {f"{self.NPCS[npc]["DESCRIPTION"]}":<90}||      {self.NPCS[npc]["TYPE"]}"
                         print(f"5-Suspeito (amigo de 3):  {npc_str}")
             case "Esfaqueado":
                 match npc:
@@ -55,7 +56,7 @@ class CASE():
             while name["genero"] != description["genero"][0]: description = random.choice(self.NPC_VALUES["DESCRICOES"])
             self.NPCS.append({"NAME":name["nome"],"DESCRIPTION":{"OCUPACAO":description["ocupacao"],"PERSONALIDADE":description["personalidade"]}, "TYPE":...})
         for npc in range(len(self.NPCS)):
-            npc_str = f"{f"{self.NPCS[npc]["NAME"]}":<20}||     {f"{self.NPCS[npc]["DESCRIPTION"]}":<90}|| {self.NPCS[npc]["TYPE"]}"
-            self.set_types(npc, npc_str)
+            self.set_types(npc)
+        self.EVIDENCES[""]
         return True
 print(CASE().generate_case())
